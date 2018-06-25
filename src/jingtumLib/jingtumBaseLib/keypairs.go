b46f84b99aaa0bedf3f6d447b22caa4617e42989
yangxuebo <yangxuebo@17of.com>
2018-06-25 09:32:52 +0800

    add crypto fix

---
+++ src/jingtumLib/jingtumBaseLib/keypairs.go
@@ -0,0 +1,19 @@
+/**
+ *
+ * 文件功能介绍
+ *
+ * @FileName: keypairs.go
+ * @Auther : 13851485286
+ * @Email : yangxuebo@yeah.net
+ * @CreateTime: 2013-09-16 10:44:32
+ * @UpdateTime: 2013-09-16 10:44:54
+ * Copyright@2013 版权所有
+ */
+
+package jingtumBaseLib
+
+
+
+func CheckAddress(address string) bool {
+    fmt.Println("Hello World")
+}
