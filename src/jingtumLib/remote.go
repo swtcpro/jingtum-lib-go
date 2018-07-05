6cd1292d579a3c7f992e39b24e15e0e6f46d8619
IPostMan <1416205324@qq.com>
2018-07-06 00:24:14 +0800

    norm remote

--- src/jingtumLib/remote.go
+++ src/jingtumLib/remote.go
@@ -3,86 +3,159 @@ package jingtumLib
 import (
 	"common/golang.org/x/net/websocket"
 	"errors"
+	//"fmt"
 	"time"
 )
 
-/*
-type jtremote interface {
-    //连接
-    connect()
-    //断开
-    disconnect()
-    //请求底层服务器信息
-    requestServerInfo()
-    //获取最新账本信息
-    requestLedgerClosed()
-    //获取某一账本具体信息
-    requestLedger(options)
-    //查询某一交易具体信息
-    requestTx(options)
-    //请求账号信息
-    requestAccountInfo(options)
-    //获得账号可接收和发送的货币
-    requestAccountTums(options)
-    //获得账号关系
-    requestAccountRelations(options)
-    //获得账号挂单
-    requestAccountOffers(options)
-    //获得账号交易列表
-    requestAccountTx(options)
-    //获得市场挂单列表
-    requestOrderBook(options)
-}
-*/
+//接收最长报文
+var (
+	MAX_RECIVE_LEN = 4096000
+)
 
-type Conn struct {
+//websocket 连接类
+type WsConn struct {
 	Ws   *websocket.Conn
 	Host string
 	Port string
 }
 
 /*
+* remote是跟井通底层交互最主要的类，它可以组装交易发送到底层、订阅事件及从底层拉取数据。
+ */
+
+type Remote struct {
+	Wsconn *WsConn
+	Params map[string]string
+	Status bool
+}
+
+/*
+* remoter 提供以下方法：
+ */
+
+type Remoter interface {
+	//连接
+	Connect() error
+	//断开
+	//获取当前时间
+	Get_now_time() string
+	// 发送
+	send(request string) error
+	// 接收
+	read() (error, string)
+	//断开连接
+	Disconnect()
+	//请求底层服务器信息
+	RequestServerInfo() (error, string)
+	//获取最新账本信息
+	RequestLedgerClosed() (error, string)
+	//获取某一账本具体信息
+	RequestLedger(ledger_index string, ledger_hash string, transactions bool) (error, string)
+	//询某一交易具体信息
+	RequestTx(hash string) (error, string)
+	//请求账号信息
+	RequestAccountInfo(account string) (error, string)
+	//得账号可接收和发送的货币
+	RequestAccountTums(account string) (error, string)
+	//得账号关系
+	RequestAccountRelations(account string, atype string) (error, string)
+	//获得账号挂单
+	RequestAccountOffers(account string) (error, string)
+	//获得账号交易列表
+	RequestAccountTx(account string, limit int) (error, string)
+	//获得市场挂单列表
+	RequestOrderBook(account string, gets string, pays string) (error, string)
+}
+
+/*
+*   构造函数 带形参
+*   params:
+*           host 主机名
+*           hort  端口号
+ */
+/*
+func NewRemote(host string, port string) (*Remote) {
+	remote := new(Remote)
+	remote.Wsconn = new(WsConn)
+	//remote.Wsconn.Ws = websocket.Conn
+	remote.Wsconn.Host = host
+	remote.Wsconn.Port = port
+	remote.Params = make(map[string]string)
+	remote.Status = false
+	return remote
+}
+*/
+/*
+*   构造函数 不带形参
+*   params:
+*           从配置文件读取
+*           Service|Host
+*           Service|Port
+ */
+func NewRemote() (error, *Remote) {
+	remote := new(Remote)
+	remote.Wsconn = new(WsConn)
+	remote.Wsconn.Host = JTConfig.Read("Service", "Host")
+	if remote.Wsconn.Host == "" {
+		Error("Config Service:Host is null.")
+		return errors.New("Config|service:Host setting error"), remote
+	}
+	remote.Wsconn.Port = JTConfig.Read("Service", "Port")
+	if remote.Wsconn.Port == "" {
+		Error("Config Service:Port is null.")
+		return errors.New("Config|service:Port setting error"), remote
+	}
+	remote.Params = make(map[string]string)
+	remote.Status = false
+	return nil, remote
+}
+
+/*
 * 连接函数
  */
-func Connect() (error, *Conn) {
-	var host_port string
-	var conn Conn
-	conn.Host = ""
-	conn.Port = ""
-	host := JTConfig.Read("Service", "Host")
-	if host == "" {
-		Error("Config Service:Host is null.")
-		return errors.New("Config|service:Host setting error"), &conn
+func (remote *Remote) Connect() error {
+	if remote.Status {
+		return nil
 	}
-	port := JTConfig.Read("Service", "Port")
-	if port == "" {
-		Error("Config Service:Port is null.")
-		return errors.New("Config|service:Port setting error"), &conn
-	}
-	host_port = host + ":" + port
+	host_port := remote.Wsconn.Host + ":" + remote.Wsconn.Port
 	origin := "http://localhost/"
 	ws, err := websocket.Dial(host_port, "", origin)
 	if err != nil {
 		Error("Connect ", host_port, "fail! errno = ", err)
 	} else {
 		Info("Connect ", host_port, " succ. ")
+		remote.Status = true
 	}
-	conn.Ws = ws
-	conn.Host = host
-	conn.Port = port
-	return err, &conn
+	remote.Wsconn.Ws = ws
+	return err
 }
 
-func Get_now_time() string {
+/*
+*   获取当前时间
+    params: 无
+    return: string
+            格式(2006-01-02 15:04:05)
+*/
+func (remote *Remote) Get_now_time() string {
 	t := time.Now()
 	return t.Format("2006-01-02 15:04:05")
 }
 
 /*
 * 发送
- */
-func send(request string, conn *Conn) error {
-	_, err := conn.Ws.Write([]byte(request))
+*     params:
+             request 待发送的报文
+*/
+func (remote *Remote) send(request string) error {
+	if !remote.Status {
+		err := remote.Connect()
+		if err != nil {
+			host_port := remote.Wsconn.Host + ":" + remote.Wsconn.Port
+			Error("Connect ", host_port, "fail! errno = ", err)
+			return err
+		}
+	}
+	_, err := remote.Wsconn.Ws.Write([]byte(request))
 	if err != nil {
 		Error("Send ", request, "fail!", "errno:", err)
 	} else {
@@ -93,12 +166,23 @@ func send(request string, conn *Conn) error {
 
 /*
 * 接收
- */
+    params: 无
+    return:
+           接收的报文
+*/
 
-func read(conn *Conn) (error, string) {
-	var msg = make([]byte, 409600)
+func (remote *Remote) read() (error, string) {
+	if !remote.Status {
+		err := remote.Connect()
+		if err != nil {
+			host_port := remote.Wsconn.Host + ":" + remote.Wsconn.Port
+			Error("Connect ", host_port, "fail! errno = ", err)
+			return err, ""
+		}
+	}
+	var msg = make([]byte, MAX_RECIVE_LEN)
 	var n int
-	n, err := conn.Ws.Read(msg)
+	n, err := remote.Wsconn.Ws.Read(msg)
 	if err != nil {
 		Error("Received data fail!", "errno:", err)
 	} else {
@@ -110,21 +194,33 @@ func read(conn *Conn) (error, string) {
 /*
 *  断开连接
  */
-func Disconnect(conn *Conn) {
-	conn.Ws.Close()
+func (remote *Remote) Disconnect() {
+	remote.Wsconn.Ws.Close()
+	remote.Status = false
 }
 
 /*
 * 请求底层服务器信息
- */
-func RequestServerInfo(conn *Conn) (error, string) {
+    return:
+           response  返回结果
+*/
+func (remote *Remote) RequestServerInfo() (error, string) {
+	if !remote.Status {
+		err := remote.Connect()
+		if err != nil {
+			host_port := remote.Wsconn.Host + ":" + remote.Wsconn.Port
+			Error("Connect ", host_port, "fail! errno = ", err)
+			return err, ""
+		}
+	}
+
 	request := Pack_RequestServerInfo()
-	err := send(request, conn)
+	err := remote.send(request)
 	if err != nil {
 		Error("Send data fail!")
 		return err, ""
 	}
-	err, response := read(conn)
+	err, response := remote.read()
 	if err != nil {
 		Error("Received data fail!")
 		return err, ""
@@ -135,15 +231,25 @@ func RequestServerInfo(conn *Conn) (error, string) {
 
 /*
 * 获取最新账本信息
+* return
  */
-func RequestLedgerClosed(conn *Conn) (error, string) {
+func (remote *Remote) RequestLedgerClosed() (error, string) {
+	if !remote.Status {
+		err := remote.Connect()
+		if err != nil {
+			host_port := remote.Wsconn.Host + ":" + remote.Wsconn.Port
+			Error("Connect ", host_port, "fail! errno = ", err)
+			return err, ""
+		}
+	}
+
 	request := Pack_RequestLedgerClosed()
-	err := send(request, conn)
+	err := remote.send(request)
 	if err != nil {
 		Error("Send data fail!")
 		return err, ""
 	}
-	err, response := read(conn)
+	err, response := remote.read()
 	if err != nil {
 		Error("Received data fail!")
 		return err, ""
@@ -155,17 +261,26 @@ func RequestLedgerClosed(conn *Conn) (error, string) {
 /*
 * 获取某一账本具体信息
  */
-func RequestLedger(conn *Conn, ledger_index string, ledger_hash string, transactions bool) (error, string) {
+func (remote *Remote) RequestLedger(ledger_index string, ledger_hash string, transactions bool) (error, string) {
+	if !remote.Status {
+		err := remote.Connect()
+		if err != nil {
+			host_port := remote.Wsconn.Host + ":" + remote.Wsconn.Port
+			Error("Connect ", host_port, "fail! errno = ", err)
+			return err, ""
+		}
+	}
+
 	if ledger_index == "" && ledger_hash == "" {
 		return errors.New("ledger_index|ledger_hash value error"), ""
 	}
 	request := Pack_RequestLedger(ledger_index, ledger_hash, transactions)
-	err := send(request, conn)
+	err := remote.send(request)
 	if err != nil {
 		Error("Send data fail!")
 		return err, ""
 	}
-	err, response := read(conn)
+	err, response := remote.read()
 	if err != nil {
 		Error("Received data fail!")
 		return err, ""
@@ -177,14 +292,22 @@ func RequestLedger(conn *Conn, ledger_index string, ledger_hash string, transact
 /*
 * 查询某一交易具体信息
  */
-func RequestTx(conn *Conn, hash string) (error, string) {
+func (remote *Remote) RequestTx(hash string) (error, string) {
+	if !remote.Status {
+		err := remote.Connect()
+		if err != nil {
+			host_port := remote.Wsconn.Host + ":" + remote.Wsconn.Port
+			Error("Connect ", host_port, "fail! errno = ", err)
+			return err, ""
+		}
+	}
 	request := Pack_RequestTx(hash)
-	err := send(request, conn)
+	err := remote.send(request)
 	if err != nil {
 		Error("Send data fail!")
 		return err, ""
 	}
-	err, response := read(conn)
+	err, response := remote.read()
 	if err != nil {
 		Error("Received data fail!")
 		return err, ""
@@ -196,14 +319,22 @@ func RequestTx(conn *Conn, hash string) (error, string) {
 /*
 * 请求账号信息
  */
-func RequestAccountInfo(conn *Conn, account string) (error, string) {
+func (remote *Remote) RequestAccountInfo(account string) (error, string) {
+	if !remote.Status {
+		err := remote.Connect()
+		if err != nil {
+			host_port := remote.Wsconn.Host + ":" + remote.Wsconn.Port
+			Error("Connect ", host_port, "fail! errno = ", err)
+			return err, ""
+		}
+	}
 	request := Pack_RequestAccountInfo(account)
-	err := send(request, conn)
+	err := remote.send(request)
 	if err != nil {
 		Error("Send data fail!")
 		return err, ""
 	}
-	err, response := read(conn)
+	err, response := remote.read()
 	if err != nil {
 		Error("Received data fail!")
 		return err, ""
@@ -215,14 +346,22 @@ func RequestAccountInfo(conn *Conn, account string) (error, string) {
 /*
 * 获得账号可接收和发送的货币
  */
-func RequestAccountTums(conn *Conn, account string) (error, string) {
+func (remote *Remote) RequestAccountTums(account string) (error, string) {
+	if !remote.Status {
+		err := remote.Connect()
+		if err != nil {
+			host_port := remote.Wsconn.Host + ":" + remote.Wsconn.Port
+			Error("Connect ", host_port, "fail! errno = ", err)
+			return err, ""
+		}
+	}
 	request := Pack_RequestAccountTums(account)
-	err := send(request, conn)
+	err := remote.send(request)
 	if err != nil {
 		Error("Send data fail!")
 		return err, ""
 	}
-	err, response := read(conn)
+	err, response := remote.read()
 	if err != nil {
 		Error("Received data fail!")
 		return err, ""
@@ -234,17 +373,25 @@ func RequestAccountTums(conn *Conn, account string) (error, string) {
 /*
 * 获得账号关系
  */
-func RequestAccountRelations(conn *Conn, account string, atype string) (error, string) {
+func (remote *Remote) RequestAccountRelations(account string, atype string) (error, string) {
+	if !remote.Status {
+		err := remote.Connect()
+		if err != nil {
+			host_port := remote.Wsconn.Host + ":" + remote.Wsconn.Port
+			Error("Connect ", host_port, "fail! errno = ", err)
+			return err, ""
+		}
+	}
 	request := Pack_RequestAccountRelations(account, atype)
 	if request == "" {
 		return errors.New("RequestAccountRelations type value error"), ""
 	}
-	err := send(request, conn)
+	err := remote.send(request)
 	if err != nil {
 		Error("Send data fail!")
 		return err, ""
 	}
-	err, response := read(conn)
+	err, response := remote.read()
 	if err != nil {
 		Error("Received data fail!")
 		return err, ""
@@ -254,16 +401,54 @@ func RequestAccountRelations(conn *Conn, account string, atype string) (error, s
 }
 
 /*
-* 获得账号交易列表
+* 获得账号挂单
  */
-func RequestAccountTx(conn *Conn, account string, limit int) (error, string) {
-	request := Pack_RequestAccountTx(account, limit)
-	err := send(request, conn)
+func (remote *Remote) RequestAccountOffers(account string) (error, string) {
+	if !remote.Status {
+		err := remote.Connect()
+		if err != nil {
+			host_port := remote.Wsconn.Host + ":" + remote.Wsconn.Port
+			Error("Connect ", host_port, "fail! errno = ", err)
+			return err, ""
+		}
+	}
+	request := Pack_RequestAccountOffers(account)
+	if request == "" {
+		return errors.New("RequestAccountRelations type value error"), ""
+	}
+	err := remote.send(request)
 	if err != nil {
 		Error("Send data fail!")
 		return err, ""
 	}
-	err, response := read(conn)
+	err, response := remote.read()
+	if err != nil {
+		Error("Received data fail!")
+		return err, ""
+	}
+	Info("Get Reqonse Account Offers succ: ", response)
+	return nil, response
+}
+
+/*
+* 获得账号交易列表
+ */
+func (remote *Remote) RequestAccountTx(account string, limit int) (error, string) {
+	if !remote.Status {
+		err := remote.Connect()
+		if err != nil {
+			host_port := remote.Wsconn.Host + ":" + remote.Wsconn.Port
+			Error("Connect ", host_port, "fail! errno = ", err)
+			return err, ""
+		}
+	}
+	request := Pack_RequestAccountTx(account, limit)
+	err := remote.send(request)
+	if err != nil {
+		Error("Send data fail!")
+		return err, ""
+	}
+	err, response := remote.read()
 	if err != nil {
 		Error("Received data fail!")
 		return err, ""
@@ -271,3 +456,58 @@ func RequestAccountTx(conn *Conn, account string, limit int) (error, string) {
 	Info("Get Reqonse Account Tx succ: ", response)
 	return nil, response
 }
+
+/*
+* 获得市场挂单列表
+ */
+func (remote *Remote) RequestOrderBook(account string, gets string, pays string) (error, string) {
+	if !remote.Status {
+		err := remote.Connect()
+		if err != nil {
+			host_port := remote.Wsconn.Host + ":" + remote.Wsconn.Port
+			Error("Connect ", host_port, "fail! errno = ", err)
+			return err, ""
+		}
+	}
+	request := Pack_RequestOrderBook(account, gets, pays)
+	err := remote.send(request)
+	if err != nil {
+		Error("Send data fail!")
+		return err, ""
+	}
+	err, response := remote.read()
+	if err != nil {
+		Error("Received data fail!")
+		return err, ""
+	}
+	Info("Get Reqonse Account Tx succ: ", response)
+	return nil, response
+}
+
+/*
+* 支付
+ */
+/*
+func (remote *Remote) BuildPaymentTx(account string) (error, string) {
+    if !remote.Status {
+        err := remote.Connect()
+        if if err != nil {
+            host_port := remote.Wsconn.Host + ":" + remote.Wsconn.Port
+            Error("Connect ", host_port, "fail! errno = ", err)
+            return err, ""
+        }
+    }
+	request := fmt.Sprintf("{\"id\":%d,\"command\":\"submit\",\"secret\":\"shXKYEcFwmxPSCeu4rRUz1ECEYtZP\",\"tx_json\":{\"Flags\":0,\"TransactionType\":\"Payment\",\"Account\":\"jD86doF9mBbAfTgK62L6mpqg4YJ1Yhm5wq\",\"Amount\":\"1000000\",\"Destination\":\"jQpP2UpTxECw74kuzrMXB6YEU3jsrnDDsc\"}}", seq)
+	err := send(request)
+	if err != nil {
+		Error("Send data fail!")
+		return err, ""
+	}
+	err, response := read()
+	if err != nil {
+		Error("Received data fail!")
+		return err, ""
+	}
+	Info("Get Reqonse Build Payment Tx succ: ", response)
+	return nil, response
+}*/
