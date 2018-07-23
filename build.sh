4b666fd932b091fb3194d49f13f80c10fda538fc
IPostMan <1416205324@qq.com>
2018-07-23 16:40:49 +0800

    rm net

--- build.sh
+++ build.sh
@@ -3,6 +3,12 @@ if [ ! -f build.sh ]; then
     echo 'install must be run within its container folder' 1>&2
     exit 1
 fi
+if [ ! -d bin ]; then
+      mkdir bin
+fi
+if [ ! -d log ]; then
+      mkdir log
+fi
 CURDIR=`pwd`
 OLDGOPATH="$GOPATH"
 export GOPATH="$CURDIR"
