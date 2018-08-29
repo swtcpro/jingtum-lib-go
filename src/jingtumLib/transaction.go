fa8ccf857e4ae7a4e6aaafa02943721d0a79ede7
yangxuebo-138 <yangxuebo@yeah.net>
2018-08-29 17:51:15 +0800

    注释规范化

--- src/jingtumLib/transaction.go
+++ src/jingtumLib/transaction.go
@@ -1,16 +1,11 @@
-/**
- * Transaction类主管POST请求，包括组装交易和交易参数。请求时需要提供密钥，且交易可以
- * 进行本地签名和服务器签名。目前支持服务器签名，本地签名支持主要的交易，还有部分参数
- * 不支持。所有的请求是异步的，会提供一个回调函数。每个回调函数有两个参数，一个是错误，
- * 另一个是结果。
- *
- * @FileName: transaction.go
- * @Auther : 杨雪波
- * @Email : yangxuebo@yeah.net
- * @CreateTime: 2018-05-28 10:44:32
- * @UpdateTime: 2018-05-28 10:44:54
- */
-
+// Package jingtumlib Transaction类主管POST请求，包括组装交易和交易参数。请求时需要提供密钥，且交易可以
+// 进行本地签名和服务器签名。目前支持服务器签名，本地签名支持主要的交易，还有部分参数
+// 不支持。所有的请求是异步的，会提供一个回调函数。每个回调函数有两个参数，一个是错误，另一个是结果。
+// @FileName: transaction.go
+// @Auther : 杨雪波
+// @Email : yangxuebo@yeah.net
+// @CreateTime: 2018-05-28 10:44:32
+// @UpdateTime: 2018-05-28 10:44:54
 package jingtumlib
 
 import (
